a4d306a2124570c14ae00bea527ce68bd80240dd
IsaDonutCat <114428942+IsaDonutCat@users.noreply.github.com>
2025-10-08 14:54:45 +0000

    updates

---
+++ script.js
@@ -0,0 +1,11 @@
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+  if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
+    document.getElementById("landing-cover").style.background = "rgba(0,0,0,0.5)";
+    document.getElementById("navbar").style.background = "rgba(134, 169, 217,0.3)";
+  } 
+  else {
+    document.getElementById("landing-cover").style.background = "rgba(0,0,0,0)";
+    document.getElementById("navbar").style.background = "rgba(134, 169, 217,0)";
+  }
+}
